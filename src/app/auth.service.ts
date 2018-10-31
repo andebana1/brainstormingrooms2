1787f36103d62796df525a7018ce67e3fc769101
hiaggoliveira <iagocomputacao@gmail.com>
2018-10-31 16:18:37 -0300

    Cadastro Usuario com Firebase

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -6,14 +6,14 @@ export class Auth{
 		//console.log('Testando servico: ', user)
 		firebase.auth().createUserWithEmailAndPassword(user.email, user.senha)
 			.then((sucesso: any) =>{
-				 console.log('Sucesso no cadastro')
+				 console.log('Sucesso no cadastro do usuario')
 				 console.log(sucesso)
 				 firebase.database().ref(`dados_do_usuario/${btoa(user.email)}`)
 					.set(user)
 			}
 			)
 			.catch((erro: Error)=>{
-			    console.log('Erro no cadastro')
+			    console.log('Erro no cadastro do usuario')
 				console.log(erro)
 			}
 			)
